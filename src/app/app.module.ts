e303d8f899100a8ecbe8369390edb7c465522314
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-16 04:12:25 +0200

    hiring app start with nav bar

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { MasterLayoutModule } from './layout/master-layout.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MasterLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
