e7f5bded527c7bae6932b4e2e40d22d36839ed2e
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-17 14:03:45 +0200

    make footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
 
 const routes: Routes = [
   {
-
+    path: '',
+    component: MainComponent,
   }
 ];
 
