8e1c68ff12145aaf899adcb8bb8cac59aa0dffa3
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-19 00:41:44 +0200

    create layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { PublicHomeComponent } from './pages/public-home/public-home.component';
+import { PublicModule } from './pages/public.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicHomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MasterLayoutModule
+    PublicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
