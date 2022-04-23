66061ae1a447013202e312b4373da72c07ad63ac
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-23 23:33:58 +0200

    add auto mapper

---
+++ HiringAPI/Hiring/Application/Mapping/Auth/Authprofile.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Dto.General.Auth;
+using Domain.Entities.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping.Auth
+{
+    internal class Authprofile:MappingProfileBase
+    {
+        public Authprofile()
+        {
+            CreateMap<ApplicationUser, RegisterDto>();
+        }
+    }
+}
